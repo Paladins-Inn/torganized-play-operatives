6b8672571088f4950ac67f11e3f8dba56f4056ec
Roland T. Lichti <rlichti@kaiserpfalz-edv.de>
2024-08-10 07:32:30 +0200

    created storm-knights SCS

---
+++ src/main/java/de/paladinsinn/tp/dcis/stormknights/Application.java
@@ -0,0 +1,31 @@
+package de.paladinsinn.tp.dcis.stormknights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+
+import jakarta.servlet.DispatcherType;
+
+/**
+ * @author Roland T. Lichti {@literal <rlichti@kaiserpfalz-edv.de>}
+ * @version 1.0.0
+ * @since 2024-06-09
+ */
+@SpringBootApplication
+public class Application extends SpringApplication {
+    public static void main(String [] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+        ForwardedHeaderFilter filter = new ForwardedHeaderFilter();
+        FilterRegistrationBean<ForwardedHeaderFilter> registration = new FilterRegistrationBean<>(filter);
+        registration.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ASYNC, DispatcherType.ERROR);
+        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return registration;
+    }
+}
