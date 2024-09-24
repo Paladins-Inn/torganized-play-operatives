97a21e9387c2565b2041898e25d188da42aa9827
Roland T. Lichti <rlichti@kaiserpfalz-edv.de>
2024-09-24 05:53:26 +0200

    one spot missed due to lazyness of not updating first ...

--- src/main/java/de/paladinsinn/tp/dcis/operatives/persistence/OperativeToJpa.java
+++ src/main/java/de/paladinsinn/tp/dcis/operatives/persistence/OperativeToJpa.java
@@ -4,13 +4,17 @@ import java.util.List;
 import java.util.function.Function;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import de.paladinsinn.tp.dcis.operatives.domain.model.Operative;
 import de.paladinsinn.tp.dcis.operatives.domain.model.OperativeImpl;
 
 @Mapper
 public interface OperativeToJpa extends Function<OperativeImpl, OperativeJPA> {
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "revId", ignore = true)
+    @Mapping(target = "revisioned", ignore = true)
     OperativeJPA apply(Operative orig);
 
-    List<OperativeJPA> apply(List<? extends Operative> orig);
+    List<OperativeJPA> map(List<? extends Operative> orig);
 }
