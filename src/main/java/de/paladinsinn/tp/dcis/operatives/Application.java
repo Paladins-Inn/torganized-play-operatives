48cd4d966cc1c85c687745411c0b3dd1df7e4e28
Roland T. Lichti <rlichti@kaiserpfalz-edv.de>
2024-09-27 01:39:15 +0200

    WIP

--- src/main/java/de/paladinsinn/tp/dcis/operatives/Application.java
+++ src/main/java/de/paladinsinn/tp/dcis/operatives/Application.java
@@ -5,8 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
+import org.springframework.data.envers.repository.config.EnableEnversRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
+import de.paladinsinn.tp.dcis.commons.configuration.EnableDefaultConfiguration;
 import jakarta.servlet.DispatcherType;
 
 /**
@@ -15,6 +20,12 @@ import jakarta.servlet.DispatcherType;
  * @since 2024-06-09
  */
 @SpringBootApplication
+@EnableDefaultConfiguration
+@EnableJpaRepositories(basePackages = "de.paladinsinn.tp.dcis.operatives.persistence")
+@EnableJpaAuditing
+@EnableEnversRepositories
+@EnableTransactionManagement
+
 public class Application extends SpringApplication {
     public static void main(String [] args) {
         SpringApplication.run(Application.class, args);
